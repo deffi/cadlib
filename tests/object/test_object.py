46351c6c9d766b5df0fda1647f8d839900554e78
Martin Herrmann <martin@famherrmann.de>
2018-10-17 20:58:57 +0200

    Cleanup unit tests

--- tests/object/test_object.py
+++ tests/object/test_object.py
@@ -9,22 +9,13 @@ from tests.unit_test import TestCase
 from cadlib.csg.intersection import Intersection
 from cadlib.csg.difference import Difference
 from cadlib.csg.union import Union
+from cadlib.transform.primitives.rotate_ypr import RotateYpr
+
 
 class TestObject(TestCase):
-    def test_multiplication_with_transform(self):
-        r = RotateXyz(60, 30, 15)
-        s = Scale([1, 2, -1])
-        cube = Cube(11)
-
-        # Simple transform
-        self.assertEqual(r * cube, Transformed(r, cube))
-
-        # Wrong order
-        with self.assertRaises(TypeError): cube * r
-
-        # Multiple transform
-        self.assertEqual(  (r *  s) * cube   , Transformed(Chained([r, s]), cube))
-        self.assertEqual(   r * (s  * cube)  , Transformed(Chained([r, s]), cube))
+    ################
+    ## Transforms ##
+    ################
 
     def test_postfix_transform(self):
         cube = Cube(11)
@@ -49,9 +40,26 @@ class TestObject(TestCase):
         with self.assertRaises(TypeError): cube.transform(cube)
 
     def test_transform_shortcuts(self):
-        cube = Cube(11)
+        a = Cube(11)
 
-        self.assertEqual(cube.up(10), Translate([0, 0, 10]) * cube)
+        self.assertEqual(a.up     (1), Transformed(Translate([ 0,  0,  1]), a))
+        self.assertEqual(a.down   (2), Transformed(Translate([ 0,  0, -2]), a))
+        self.assertEqual(a.left   (3), Transformed(Translate([-3,  0,  0]), a))
+        self.assertEqual(a.right  (4), Transformed(Translate([ 4,  0,  0]), a))
+        self.assertEqual(a.forward(5), Transformed(Translate([ 0,  5,  0]), a))
+        self.assertEqual(a.back   (6), Transformed(Translate([ 0, -6,  0]), a))
+
+        self.assertEqual(a.yaw_left  (1), Transformed(RotateYpr( 1,  0,  0), a))
+        self.assertEqual(a.yaw_right (2), Transformed(RotateYpr(-2,  0,  0), a))
+        self.assertEqual(a.pitch_up  (3), Transformed(RotateYpr( 0,  3,  0), a))
+        self.assertEqual(a.pitch_down(4), Transformed(RotateYpr( 0, -4,  0), a))
+        self.assertEqual(a.roll_right(5), Transformed(RotateYpr( 0,  0,  5), a))
+        self.assertEqual(a.roll_left (6), Transformed(RotateYpr( 0,  0, -6), a))
+
+
+    ###############
+    ## Operators ##
+    ###############
 
     def test_addition(self):
         a = Sphere(2)
@@ -59,14 +67,14 @@ class TestObject(TestCase):
         c = Cylinder(Z, 5, 5)
         d = Cube(20)
 
-        self.assertEqual(   a +  b                 , Union([a, b      ]))
-        self.assertEqual(   c +  c                 , Union([c, c      ]))
-        self.assertEqual(  (a +  b) + c            , Union([a, b, c   ]))
-        self.assertEqual(   a + (b  + c)           , Union([a, b, c   ]))
-        self.assertEqual(    a  +  b  +  c  + d    , Union([a, b, c, d]))
-        self.assertEqual(  ((a  +  b) +  c) + d    , Union([a, b, c, d]))
-        self.assertEqual(    a  + (b  + (c  + d))  , Union([a, b, c, d]))
-        self.assertEqual(   (a  +  b) + (c  + d)   , Union([a, b, c, d]))
+        self.assertEqual(   a +  b               , Union([a, b      ]))
+        self.assertEqual(   c +  c               , Union([c, c      ]))
+        self.assertEqual(  (a +  b) + c          , Union([a, b, c   ]))
+        self.assertEqual(   a + (b  + c)         , Union([a, b, c   ]))
+        self.assertEqual(    a  +  b  +  c  + d  , Union([a, b, c, d]))
+        self.assertEqual(  ((a  +  b) +  c) + d  , Union([a, b, c, d]))
+        self.assertEqual(    a  + (b  + (c  + d)), Union([a, b, c, d]))
+        self.assertEqual(   (a  +  b) + (c  + d) , Union([a, b, c, d]))
 
     def test_multiplication(self):
         a = Sphere(2)
@@ -74,40 +82,29 @@ class TestObject(TestCase):
         c = Cylinder(Z, 5, 5)
         d = Cube(20)
 
-        self.assertEqual(    a  *  b               , Intersection([a, b      ]))
-        self.assertEqual(    c  *  c               , Intersection([c, c      ]))
-        self.assertEqual(   (a  *  b) * c          , Intersection([a, b, c   ]))
-        self.assertEqual(    a  * (b  * c)         , Intersection([a, b, c   ]))
-        self.assertEqual(    a  *  b  *  c  * d    , Intersection([a, b, c, d]))
-        self.assertEqual(  ((a  *  b) *  c) * d    , Intersection([a, b, c, d]))
-        self.assertEqual(    a  * (b  * (c  * d))  , Intersection([a, b, c, d]))
-        self.assertEqual(   (a  *  b) * (c  * d)   , Intersection([a, b, c, d]))
+        self.assertEqual(    a  *  b             , Intersection([a, b      ]))
+        self.assertEqual(    c  *  c             , Intersection([c, c      ]))
+        self.assertEqual(   (a  *  b) * c        , Intersection([a, b, c   ]))
+        self.assertEqual(    a  * (b  * c)       , Intersection([a, b, c   ]))
+        self.assertEqual(    a  *  b  *  c  * d  , Intersection([a, b, c, d]))
+        self.assertEqual(  ((a  *  b) *  c) * d  , Intersection([a, b, c, d]))
+        self.assertEqual(    a  * (b  * (c  * d)), Intersection([a, b, c, d]))
+        self.assertEqual(   (a  *  b) * (c  * d) , Intersection([a, b, c, d]))
 
-    def test_subtractino(self):
+    def test_subtraction(self):
         a = Sphere(2)
         b = Cube([10, 10, 10])
         c = Cylinder(Z, 5, 5)
         d = Cube(20)
 
-        self.assertEqual(    a  -  b               , Difference([a, b      ]))
-        self.assertEqual(    c  -  c               , Difference([c, c      ]))
-        self.assertEqual(   (a  -  b) - c          , Difference([a, b, c   ]))
-        self.assertEqual(    a  - (b  - c)         , Difference([a, Difference([b, c])]))
-        self.assertEqual(    a  -  b  -  c  - d    , Difference([a, b, c, d]))
-        self.assertEqual(  ((a  -  b) -  c) - d    , Difference([a, b, c, d]))
-        self.assertEqual(    a  - (b  - (c  - d))  , Difference([a, Difference([b, Difference([c, d])])]))
-        self.assertEqual(   (a  -  b) - (c  - d)   , Difference([Difference([a, b]), Difference([c, d])]))
-
-    def test_invalid_operations(self):
-        a = Sphere(2)
-
-        for invalid in [None, 0, 0.0, ""]:
-            with self.assertRaises(TypeError): a + invalid
-            with self.assertRaises(TypeError): a - invalid
-            with self.assertRaises(TypeError): a * invalid
-            with self.assertRaises(TypeError): invalid + a
-            with self.assertRaises(TypeError): invalid - a
-            with self.assertRaises(TypeError): invalid * a
+        self.assertEqual(    a  -  b             , Difference([a, b      ]))
+        self.assertEqual(    c  -  c             , Difference([c, c      ]))
+        self.assertEqual(   (a  -  b) - c        , Difference([a, b, c   ]))
+        self.assertEqual(    a  - (b  - c)       , Difference([a, Difference([b, c])]))
+        self.assertEqual(    a  -  b  -  c  - d  , Difference([a, b, c, d]))
+        self.assertEqual(  ((a  -  b) -  c) - d  , Difference([a, b, c, d]))
+        self.assertEqual(    a  - (b  - (c  - d)), Difference([a, Difference([b, Difference([c, d])])]))
+        self.assertEqual(   (a  -  b) - (c  - d) , Difference([Difference([a, b]), Difference([c, d])]))
 
     def test_operators_with_transforms(self):
         a = Sphere(2)
@@ -125,7 +122,22 @@ class TestObject(TestCase):
         # Multiplication is used for both intersection (Object * Object) and transform (Transform * Object)
         a = Sphere(2)
         b = Cube(3)
+        s = Scale([1, 2, -1])
         t = Translate([0, 0, 0])
 
         self.assertEqual( t * (a  * b), Transformed(t, Intersection([a, b])))
         self.assertEqual((t *  a) * b , Intersection([Transformed(t, a), b]))
+        self.assertEqual((s *  t) * a , Transformed(Chained([s, t]), a))
+        self.assertEqual( s * (t  * a), Transformed(Chained([s, t]), a))
+
+
+    def test_invalid_operations(self):
+        a = Sphere(2)
+
+        for invalid in [None, 0, 0.0, ""]:
+            with self.assertRaises(TypeError): a + invalid
+            with self.assertRaises(TypeError): a - invalid
+            with self.assertRaises(TypeError): a * invalid
+            with self.assertRaises(TypeError): invalid + a
+            with self.assertRaises(TypeError): invalid - a
+            with self.assertRaises(TypeError): invalid * a
