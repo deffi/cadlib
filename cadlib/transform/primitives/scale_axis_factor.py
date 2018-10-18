553b705f67c9e23ec425bd70dcac214e6fded5d8
Martin Herrmann <martin@famherrmann.de>
2018-10-18 22:52:25 +0200

    Add ScaleAxisFactor transform

---
+++ cadlib/transform/primitives/scale_axis_factor.py
@@ -0,0 +1,53 @@
+from cadlib.scad.scad import ScadObject
+from cadlib.transform.transform import Transform
+from cadlib.util.number import to_number
+from cadlib.util import to_vector
+from cadlib.util.vector import X, Y, Z
+from cadlib.transform.generators import rotate
+from cadlib.transform.primitives.scale_xyz import ScaleXyz
+
+class ScaleAxisFactor(Transform):
+    def __init__(self, axis, factor):
+        axis = to_vector(axis, "axis", 3)
+        if axis.length == 0:
+            raise ValueError("axis may not be zero-length")
+
+        self._axis   = axis
+        self._factor = to_number(factor, self._axis.length, "factor")
+
+    def __eq__(self, other):
+        return isinstance(other, ScaleAxisFactor) \
+            and other._axis == self._axis \
+            and other._factor == self._factor
+
+    def __str__(self):
+        return "Scale along {} by {}".format(self._axis, self._factor)
+
+    def to_scad(self, target):
+        # Since OpenSCAD does not have axis/factor scaling, it has to be
+        # translated to corresponding XYZ scales, potentially combined with
+        # rotations.
+
+        children = [target] if target is not None else []
+
+        # Special case: if the factor is 1, the scale can be expressed as the
+        # unit scale
+        if self._factor == 1:
+            return ScadObject("scale", [[1, 1, 1]], None, children)
+
+        # Special case: if the axis is aligned with one of the coordinate axes,
+        # the scale can be expressed as an XYZ scale along that axis.
+        if self._axis.collinear(X):
+            return ScadObject("scale", [[self._factor, 1, 1]], None, children)
+        if self._axis.collinear(Y):
+            return ScadObject("scale", [[1, self._factor, 1]], None, children)
+        if self._axis.collinear(Z):
+            return ScadObject("scale", [[1, 1, self._factor]], None, children)
+
+        # General case
+        transform_axis = self._axis.closest_axis()
+        equivalent = rotate(frm = self._axis, to = transform_axis)
+        equivalent = ScaleXyz((transform_axis * self._factor).values) * equivalent
+        equivalent = rotate(frm = transform_axis, to = self._axis) * equivalent
+
+        return equivalent.to_scad(target)
---
+++ tests/transform/test_scale_axis_factor.py
@@ -0,0 +1,58 @@
+from tests.unit_test import TestCase
+from cadlib.transform.primitives.scale_axis_factor import ScaleAxisFactor
+from cadlib.util import Vector
+from cadlib.scad.scad import ScadObject
+from cadlib.util.vector import X, Y, Z
+
+class TestScaleAxisFactor(TestCase):
+    def test_construction(self):
+        # Valid
+        ScaleAxisFactor(      [1, 2, 3], 4)
+        ScaleAxisFactor(Vector(1, 2, 3), 4)
+        # TODO zero factor is OK, if useless
+
+        # Invalid
+        # TODO zero vector
+        with self.assertRaises(TypeError): ScaleAxisFactor([1, 2, "3"],  4)
+        with self.assertRaises(TypeError): ScaleAxisFactor([1, 2,  3 ], "4")
+        with self.assertRaises(TypeError): ScaleAxisFactor(1          , "4")
+
+    def test_equality(self):
+        # Same object
+        self.assertEqualToItself(ScaleAxisFactor([1, 2, 3], 4))
+
+        # Equal objects
+        self.assertEqual(ScaleAxisFactor([1, 2, 3], 4), ScaleAxisFactor([1, 2, 3], 4))
+
+        # Different objects
+        self.assertNotEqual(ScaleAxisFactor([1, 2, 3], 4), ScaleAxisFactor([1, 2, 4], 5))
+        self.assertNotEqual(ScaleAxisFactor([1, 2, 3], 4), ScaleAxisFactor([1, 2, 3], 5))
+
+        # Equal objects from different specifications
+        self.assertEqual(ScaleAxisFactor(Vector(1, 2, 3), 4), ScaleAxisFactor([1, 2, 3], 4))
+
+    def test_to_scad(self):
+        # Special case: axis aligned with one of the coordinate axes
+        # X
+        self.assertEqual(ScaleAxisFactor(X, 2).to_scad(None),
+            ScadObject("scale", [[2, 1, 1]], None, None))
+        # Y
+        self.assertEqual(ScaleAxisFactor(Y, 3).to_scad(None),
+            ScadObject("scale", [[1, 3, 1]], None, None))
+        # Z
+        self.assertEqual(ScaleAxisFactor(Z, 4).to_scad(None),
+            ScadObject("scale", [[1, 1, 4]], None, None))
+        # -Z
+        self.assertEqual(ScaleAxisFactor(-Z, 4).to_scad(None),
+            ScadObject("scale", [[1, 1, 4]], None, None))
+
+        # Special case: factor 1 (aligned or non-aligned)
+        self.assertEqual(ScaleAxisFactor(X, 1).to_scad(None),
+            ScadObject("scale", [[1, 1, 1]], None, None))
+        self.assertEqual(ScaleAxisFactor(Vector(1, 1, 0), 1).to_scad(None),
+            ScadObject("scale", [[1, 1, 1]], None, None))
+
+        # Now the general case is just too ridiculous to test, but we should
+        # expect it to execute without raising an exception.
+        ScaleAxisFactor(Vector(1, 2,  3), 4).to_scad(None)
+        ScaleAxisFactor(Vector(1, 2, -3), 4).to_scad(None)
