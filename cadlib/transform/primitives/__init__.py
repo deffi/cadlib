41605c68f534b8fd634ac8a38e3667eba26cddfa
Martin Herrmann <martin@famherrmann.de>
2018-10-21 00:05:08 +0200

    Update comment

--- cadlib/transform/primitives/__init__.py
+++ cadlib/transform/primitives/__init__.py
@@ -1,2 +1,2 @@
 # Where a vector is appropriate, a list of 3 numbers will also be accepted. Note that, in particular, this does not
-# apply to RotateXyz and RotateRpy, whose arguments are not vectors.
+# apply to ScaleXyz, RotateXyz and RotateRpy, whose arguments are not vectors.
