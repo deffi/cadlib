36ac054b29de1dbb59fc107c352caa4252bef555
Martin Herrmann <martin@famherrmann.de>
2018-10-20 11:23:54 +0200

    Add ScaleUniform

---
+++ cadlib/transform/primitives/scale_uniform.py
@@ -0,0 +1,18 @@
+from cadlib.scad.scad import ScadObject
+from cadlib.transform.transform import Transform
+from cadlib.util.number import to_number
+
+class ScaleUniform(Transform):
+    def __init__(self, factor):
+        self._factor = to_number(factor, 1, "factor")
+
+    def __eq__(self, other):
+        return isinstance(other, ScaleUniform) and other._factor == self._factor
+
+    def __str__(self):
+        return "Uniform scale by {}".format(self._factor)
+
+    def to_scad(self, target):
+        children = [target] if target is not None else []
+        f = self._factor
+        return ScadObject("scale", [[f, f, f]], None, children)
---
+++ tests/transform/test_scale_uniform.py
@@ -0,0 +1,26 @@
+from tests.unit_test import TestCase
+from cadlib.transform.primitives.scale_uniform import ScaleUniform
+
+class TestScaleUniform(TestCase):
+    def test_construction(self):
+        # Valid
+        ScaleUniform(2)
+
+        # Invalid
+        with self.assertRaises(TypeError): ScaleUniform([1, 2, 3])
+        with self.assertRaises(TypeError): ScaleUniform("2")
+
+    def test_equality(self):
+        # Same object
+        self.assertEqualToItself(ScaleUniform(2))
+
+        # Equal objects
+        self.assertEqual(ScaleUniform(2), ScaleUniform(2))
+
+        # Different objects
+        self.assertNotEqual(ScaleUniform(2), ScaleUniform(3))
+
+    def test_to_scad(self):
+        s = ScaleUniform(2)
+
+        self.assertScadObjectTarget(s, None, "scale", [[2, 2, 2]], None, None)
