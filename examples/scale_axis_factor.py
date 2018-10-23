f5f1767c3d2ebc94fa39eaaca64e2f48ddf4ea80
Martin Herrmann <martin@famherrmann.de>
2018-10-23 23:30:24 +0200

    Fix output file name

--- examples/scale_axis_factor.py
+++ examples/scale_axis_factor.py
@@ -3,4 +3,4 @@ from cadlib.simple import *
 cube = Cube(20)
 tf = scale(axis=[3, 2, 1], factor=2)
 
-render_to_file(tf*cube, "rotate_axis_angle.scad")
+render_to_file(tf*cube, "scale_axis_factor.scad")
