caab8c5cb7ea82f7bc820a6debfea64f4c3259ad
Martin Herrmann <martin@famherrmann.de>
2018-10-25 23:36:50 +0200

    Factor out transform shortcut tests from test_transform.py

--- tests/transform/test_transform.py
+++ tests/transform/test_transform.py
@@ -1,7 +1,6 @@
 from cadlib.transform.primitives import Translate, ScaleXyz, RotateXyz, RotateYpr
 from cadlib.transform.chained import Chained
 from tests.unit_test import TestCase
-from cadlib.transform import shortcuts
 
 class TestTransform(TestCase):
     def test_inequality(self):
@@ -59,24 +58,3 @@ class TestTransform(TestCase):
             with self.assertRaises(TypeError): t - other
             with self.assertRaises(TypeError): other + t
             with self.assertRaises(TypeError): other - t
-
-
-    ###############
-    ## Shortcuts ##
-    ###############
-
-    def test_translate_shortcuts(self):
-        self.assertEqual(shortcuts.up     (1), Translate([ 0,  0,  1]))
-        self.assertEqual(shortcuts.down   (2), Translate([ 0,  0, -2]))
-        self.assertEqual(shortcuts.left   (3), Translate([-3,  0,  0]))
-        self.assertEqual(shortcuts.right  (4), Translate([ 4,  0,  0]))
-        self.assertEqual(shortcuts.forward(5), Translate([ 0,  5,  0]))
-        self.assertEqual(shortcuts.back   (6), Translate([ 0, -6,  0]))
-
-    def test_rotate_shortcuts(self):
-        self.assertScadCode(shortcuts.yaw_left  (1), "rotate([0, 0, 1]);")
-        self.assertScadCode(shortcuts.yaw_right (2), "rotate([0, 0, -2]);")
-        self.assertScadCode(shortcuts.pitch_up  (3), "rotate([3, 0, 0]);")
-        self.assertScadCode(shortcuts.pitch_down(4), "rotate([-4, 0, 0]);")
-        self.assertScadCode(shortcuts.roll_right(5), "rotate([0, 5, 0]);")
-        self.assertScadCode(shortcuts.roll_left (6), "rotate([0, -6, 0]);")
