b7461c99ac2a7dfa62eba9fd23dd3d31877ae673
Martin Herrmann <martin@famherrmann.de>
2018-10-27 21:11:10 +0200

    Extend scale_axis_factor and rename to scale_example

---
+++ examples/scale_example.py
@@ -0,0 +1,26 @@
+from cadlib.simple import *
+
+cube = cube(20, 15, 10)
+
+uniform = [
+    scale(1),
+    scale(2),
+]
+xyz = [
+    scale(xyz = [2, 2, 2]),
+    scale(xyz = [1/2, 1/1.5, 1]),
+]
+axis_factor = [
+    scale(axis=[3, 2, 1], factor=2),
+    scale(axis=[3, 0, 0], factor=2),
+]
+
+groups = [uniform, xyz, axis_factor]
+
+assembly = union()
+for i, group in enumerate(groups):
+    for j, tf in enumerate(group):
+        assembly = assembly + (tf * cube).forward(i*50).right(j*50)
+
+
+render_to_file(assembly, "scale_example.scad")
