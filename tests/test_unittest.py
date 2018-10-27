7e4a53fcb0646434e4f7c4e8db1baab071679e14
Martin Herrmann <martin@famherrmann.de>
2018-10-27 21:37:14 +0200

    Test assertAlmostEqual

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,6 +1,6 @@
 from tests.unit_test import TestCase
-from cadlib.object.primitives.sphere import Sphere
 from cadlib.scad import ScadObject
+from cadlib.util import Matrix
 
 class Foo(Exception):
     pass
@@ -50,3 +50,41 @@ class TestUnitTest(TestCase):
         self.ignore_scad_comments = False
         with self.assertRaises(AssertionError):
             self.assertEqual(scad1, scad2)
+
+    def test_assert_almost_equal_list(self):
+        epsilon = 1e-8
+
+        # Almost equal
+        self.assertAlmostEqual([1], [1 + epsilon])
+        self.assertAlmostEqual([1, 2], [1 + epsilon, 2 + epsilon])
+        self.assertAlmostEqual([[1, 2], [3, 4]], [[1 + epsilon, 2 + epsilon], [3 + epsilon, 4 + epsilon]])
+
+        # Not almost equal
+        with self.assertRaises(AssertionError):
+            self.assertAlmostEqual([1, 2], [1.1, 2.1])
+
+        # Not if the dimensions are different
+        with self.assertRaises(AssertionError):
+            self.assertAlmostEqual([1, 2], [1, 2, 3])
+
+    def test_assert_almost_equal_matrix(self):
+        epsilon = 1e-8
+        delta   = 0.1
+
+        m1 = Matrix.from_rows([1, 2], [3, 4])
+        m2 = Matrix.from_rows([1 + epsilon, 2 + epsilon], [3 + epsilon, 4 + epsilon])
+        m3 = Matrix.from_rows([1 + delta  , 2 + delta  ], [3 + delta  , 4 + delta  ])
+        m4 = Matrix.from_rows([1, 2], [3, 4], [5, 6])
+
+        # Almost equal
+        self.assertAlmostEqual(m1, m1)
+        self.assertAlmostEqual(m1, m2)
+
+        # Not almost equal
+        with self.assertRaises(AssertionError):
+            self.assertAlmostEqual(m1, m3)
+
+        # Not if the dimensions are different
+        with self.assertRaises(AssertionError):
+            self.assertAlmostEqual(m1, m4)
+
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -61,17 +61,12 @@ class TestCase(OriginalTestCase):
         self.assertAlmostEqual(b.to_matrix() * a.to_matrix(), Matrix.identity(4))
 
     def assertAlmostEqual(self, first, second, places=None, msg=None, delta=None):
-        # TODO enable and test
-        # TODO remove by adding Matrix.abs?
-        # TODO better message
         if isinstance(first, Matrix) and isinstance(second, Matrix):
             self.assertEqual(first.dimensions, second.dimensions)
             for r in range(first.row_count):
                 for c in range(first.column_count):
                     super().assertAlmostEqual(first[r, c], second[r, c], places, msg, delta)
         elif isinstance(first, list) and isinstance(second, list):
-            # TODO test
-            # TODO better message
             self.assertEqual(len(first), len(second))
             for a, b in zip(first, second):
                 self.assertAlmostEqual(a, b)
