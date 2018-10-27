a199c9b0a8ecef35d2823764a22c7f29d7f76a90
Martin Herrmann <martin@famherrmann.de>
2018-10-27 19:03:08 +0200

    Use "none" as default value for t

--- cadlib/util/geometry.py
+++ cadlib/util/geometry.py
@@ -2,7 +2,8 @@ from cadlib.util import Matrix
 import math
 
 # TODO test
-def affine_matrix(x, y, z, t = [0, 0, 0]):
+def affine_matrix(x, y, z, t = None):
+    t = t or [0, 0, 0]
     return Matrix(rows = [
         [x[0], y[0], z[0], t[0]],
         [x[1], y[1], z[1], t[1]],
