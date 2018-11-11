3851264ebae9bd9f27fec0ca55af1bb670abd6fd
Martin Herrmann <martin@famherrmann.de>
2018-11-11 18:11:38 +0100

    Clean up object generators tests

--- tests/object/test_object_generators.py
+++ tests/object/test_object_generators.py
@@ -4,7 +4,7 @@ from cadlib.util import Vector, X, Y, Z
 from tests.unit_test import TestCase
 
 class TestObjectGenerators(TestCase):
-    def test_csg_generators(self):
+    def test_cuboid_generators(self):
         self.assertEqual(cuboid(1), Cuboid(1))
         self.assertEqual(cuboid([1, 2, 3]), Cuboid([1, 2, 3]))
         self.assertEqual(cuboid(1, 2, 3), Cuboid([1, 2, 3]))
@@ -13,13 +13,21 @@ class TestObjectGenerators(TestCase):
         with self.assertRaises(ValueError): cuboid(1, 2, None)
         with self.assertRaises(ValueError): cuboid(1, None, 2)
 
+    def test_frustum_generators(self):
+        pass
+
+
+    def test_cylinder_generators(self):
         self.assertEqual(cylinder(X, Y, r = 2), Cylinder(X, Y, r = 2))
         self.assertEqual(cylinder(X, 1, d = 3), Cylinder(X, 1, d = 3))
 
-        self.assertEqual(plane(X, 1), Plane(X, 1))
-
-        self.assertEqual(slice(X, 1, 2), Slice(X, 1, 2))
-
+    def test_sphere_generators(self):
         self.assertEqual(sphere(2), Sphere(2))
         self.assertEqual(sphere(r = 2), Sphere(r = 2))
         self.assertEqual(sphere(d = 2), Sphere(d = 2))
+
+    def test_plane_generators(self):
+        self.assertEqual(plane(X, 1), Plane(X, 1))
+
+    def test_slice_generators(self):
+        self.assertEqual(slice(X, 1, 2), Slice(X, 1, 2))
