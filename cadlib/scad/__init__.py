5af42045819c78a0e01bd298d669fe92c0352150
Martin Herrmann <martin@famherrmann.de>
2019-01-07 23:10:01 +0100

    Documentation

--- cadlib/scad/__init__.py
+++ cadlib/scad/__init__.py
@@ -1,2 +1,4 @@
+# Modules related to OpenSCAD
+
 from .scad_file import ScadFile
 from .scad_object import ScadObject, render_to_file
--- cadlib/simple.py
+++ cadlib/simple.py
@@ -1,3 +1,13 @@
+"""The recommended import for most applications.
+
+Most applications will want to do
+    import cadlib.simple as cl
+or
+    from cadlib.simple import *
+
+This module contains all generators (for objects, CSGs, and transforms), all
+transform shortcuts, base vectors X, Y, and Z, and render_to_file.
+"""
 from cadlib.util.vector import X, Y, Z
 from cadlib.object.generators import *
 from cadlib.csg.generators import *
