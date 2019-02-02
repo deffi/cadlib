b2408059040a75ddf5352d1f7e93613e7b1cab44
Martin Herrmann <martin@famherrmann.de>
2019-02-03 00:13:55 +0100

    Docstrings

--- cadlib/object/generators.py
+++ cadlib/object/generators.py
@@ -9,6 +9,11 @@ __all__ = ['cone', 'cuboid', 'cube', 'cylinder', 'frustum', 'plane', 'layer', 's
 
 
 def _get_radius(r, d):
+    """Given either a radius or a diameter, determine the radius.
+
+    Either r or d, but not both, must be given (non-None). Otherwise, an
+    exception will be raised. The given value is also converted to a number.
+    """
     if both(r, d):
         raise TypeError("radius and diameter cannot be specified together")
     elif r is not None:
@@ -18,7 +23,11 @@ def _get_radius(r, d):
     else:
         raise TypeError("radius or diameter must be specified")
 
+
 def _get_radii(r, d):
+    """Like _get_radius, but the given value for r or d must either be a tuple
+    or a list and must have exactly 2 elements.
+    """
     if both(r, d):
         raise TypeError("radii and diameters cannot be specified together")
     elif r is not None:
@@ -40,12 +49,19 @@ def _get_radii(r, d):
 
 
 def cuboid(size_or_x_or_xyz, y = None, z = None):
-    # Signatures (convenience forms only):
-    #   * cuboid(size)
-    #   * cuboid(x, y, z)
-    #   * cuboid(xyz)
-    # xzy can be Vector, list, or tuple. Note that a Vector can be used for xyz
-    # even though xyz is not strictly a vector.
+    """Generate a cuboid.
+
+    The cuboid will have one corner at the origin, will be aligned with the
+    axes, and extend in the positive axis directions.
+
+    Signatures (convenience forms only):
+      * cuboid(size)
+      * cuboid(x, y, z)
+      * cuboid(xyz)
+
+    xzy can be Vector, list, or tuple. Note that for convenience, a Vector can
+    be used for xyz even though xyz is not strictly a vector.
+    """
 
     if both(y, z):
         return Cuboid(size_or_x_or_xyz, y, z)
@@ -58,19 +74,28 @@ def cuboid(size_or_x_or_xyz, y = None, z = None):
 
 
 def cube(size):
-    # Signatures (convenience forms only):
-    #   * cube(size)
+    """Generate a cube.
+
+    The cube will have one corner at the origin, will be aligned with the
+    axes, and extend in the positive axis directions.
+
+    Signatures (convenience forms only):
+      * cube(size)
+    """
 
     size = number.convert(size, "size")
     return Cuboid(size, size, size)
 
 
 def cylinder(direction_or_base, length_or_cap, r = None, d = None):
-    # Signatures (convenience forms only):
-    #   * cylinder (direction, length, radius)
-    #   * cylinder (direction, length, d = diameter)
-    #   * cylinder (base, cap, radius)
-    #   * cylinder (base, cap, d = diameter)
+    """Generate a cylinder.
+
+    Signatures (convenience forms only):
+      * cylinder(direction, length, radius)
+      * cylinder(direction, length, d = diameter)
+      * cylinder(base, cap, radius)
+      * cylinder(base, cap, d = diameter)
+    """
 
     # Radius or diameter
     radius = _get_radius(r, d)
@@ -89,11 +114,18 @@ def cylinder(direction_or_base, length_or_cap, r = None, d = None):
 
 
 def cone(direction_or_base, length_or_cap, r = None, d = None):
-    # Signatures (convenience forms only):
-    #   * cone (direction, length, radius)
-    #   * cone (direction, length, d = diameter)
-    #   * cone (base, cap, radius)
-    #   * cone (base, cap, d = diameter)
+    """Generate a cone.
+
+    For the forms with a direction, the base will be at the origin.
+
+    Signatures (convenience forms only):
+      * cone(direction, length, radius)
+      * cone(direction, length, d = diameter)
+      * cone(base, cap, radius)
+      * cone(base, cap, d = diameter)
+    """
+    # TODO should use r = None, *, d = None? (not just here)
+    # TODO should be tip instead of cap
 
     # Radius or diameter
     radius = _get_radius(r, d)
@@ -110,12 +142,18 @@ def cone(direction_or_base, length_or_cap, r = None, d = None):
     else:
         raise TypeError("Invalid call signature: length_or_cap must be a vector type or a number")
 
+
 def frustum(direction_or_base, length_or_cap, r = None, d = None):
-    # Signatures (convenience forms only):
-    #   * frustum (direction, length, radius)
-    #   * frustum (direction, length, d = diameter)
-    #   * frustum (base, cap, radius)
-    #   * frustum (base, cap, d = diameter)
+    """Generate a frustum.
+
+    For the forms with a direction, the base will be at the origin.
+
+    Signatures (convenience forms only):
+      * frustum (direction, length, radii)
+      * frustum (direction, length, d = diameters)
+      * frustum (base, cap, radii)
+      * frustum (base, cap, d = diameters)
+    """
 
     # Radius or diameter
     radii = _get_radii(r, d)
@@ -134,13 +172,39 @@ def frustum(direction_or_base, length_or_cap, r = None, d = None):
 
 
 def plane(normal, offset):
+    """Generate a plane or, more precisely, a half-space.
+
+    The normal vector points outside. If offset is 0, the plane will contain the
+    origin. Otherwise, the plane will be translated by offset along the normal
+    vector.
+
+    The normal vector needs not be normalized.
+    """
     return Plane(normal, offset)
 
 
 def layer(normal, offset1, offset2):
+    """Generate a layer, i. e. a region of space between two planes.
+
+    If either offset is 0, the respective plane will contain the origin.
+    Otherwise, the plane will be translated by offset along the normal vector.
+
+    The normal vector needs not be normalized.
+    """
     return Layer(normal, offset1, offset2)
 
 
 def sphere(r = None, d = None):
+    """Generate a sphere.
+
+    Signatures (canonical forms):
+      * sphere(r = radius)
+      * sphere(d = diameter)
+
+    Signatures (convenience forms):
+      * sphere(radius)
+
+    The sphere will be centered at the origin.
+    """
     r = _get_radius(r, d)
     return Sphere(r)
--- cadlib/util/__init__.py
+++ cadlib/util/__init__.py
@@ -6,7 +6,9 @@ from .vector import Vector, X, Y, Z, origin
 degree = math.pi / 180
 
 def both(a, b):
+    """Returns True if both a and b are non-None, or False otherwise"""
     return a is not None and b is not None
 
 def neither(a, b):
+    """Returns True if both a and b are None, or False otherwise"""
     return a is None and b is None
